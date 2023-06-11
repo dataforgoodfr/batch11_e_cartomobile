15d9dd85b75db5c0bc8fc7494ff7f160c1ae2152
Bastien <bastien.gauthier@ntymail.com>
2023-06-11 19:28:08 +0200

    Ajout du graphe de répartition des puissances

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from pathlib import Path
 
 import streamlit as st
 
-from e_cartomobile.data_extract.bornes import get_bornes_data
+from e_cartomobile.data_extract.bornes import get_bornes_data, get_bornes_power_data
 
 from e_cartomobile.data_analytics.vizualisation_plotly import (
     graph_connector_types,
     graph_station_types,
     graph_stations_evolution,
+    graph_station_power_repartition
 )
 
 st.set_page_config(
@@ -29,22 +30,29 @@ st.write("## Quelques chiffres de contexte")
 
 tab1, tab2, tab3 = st.tabs(
     [
-        "Evolution du parc automobile",
         "Les Bornes de recharges actuelles",
+        "Evolution du parc automobile",
         "Quelques chiffres internationaux",
     ]
 )
 
-with tab1:
-    st.write(
-        "### En chantier"
-    )  # TODO : evolution temporelle du parc - cf immatriculations
 
-with tab2:
+with tab1:
     gdf_irve = get_bornes_data()
 
     with st.expander("Répartion des puissances", expanded=False):
-        st.write("### En chantier")
+
+        df_irve_power = get_bornes_power_data() 
+        fig_power = graph_station_power_repartition(df_irve_power)
+        st.plotly_chart(fig_power)
+
+        st.info("""
+On répartit les bornes classiquement en quatre catégories de puissance :
+1. Low : < 7,4 kW, pour les deux roues
+2. Standard : 7,4 à 22 kW, pour une petite recharge d'appoint
+3. Fast : 22 à 150 kW, pour un recharge rapide 
+4. Very Fast : > 150 kW, pour une recharge très haute puissance
+""")
 
     with st.expander("Evolution des points de recharge", expanded=False):
         fig1, fig2 = graph_stations_evolution()
@@ -56,6 +64,11 @@ with tab2:
 
     with st.expander("Types de connecteurs", expanded=False):
         st.plotly_chart(graph_connector_types(gdf_irve))
+
+with tab2:
+    st.write(
+        "### En chantier"
+    )  # TODO : evolution temporelle du parc - cf immatriculations
 
 with tab3:
     with st.expander("Europe", expanded=False):
