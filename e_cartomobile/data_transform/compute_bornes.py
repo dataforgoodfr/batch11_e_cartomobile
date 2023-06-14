8fac8507c6fb284ea9907d1ffa58f386a43ce7e2
Bastien <bastien.gauthier@ntymail.com>
2023-06-14 23:18:23 +0200

    Implement uniform scenarii

--- e_cartomobile/data_transform/compute_bornes.py
+++ e_cartomobile/data_transform/compute_bornes.py
@@ -1,43 +1,49 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from e_cartomobile.data_extract.bornes import get_bornes_data
 from e_cartomobile.data_extract.communes import get_communes_data
-from e_cartomobile.data_transform.compute_score_4 import score_4_target_commune # same distance collection
+from e_cartomobile.data_transform.compute_score_4 import (  # same distance collection
+    score_4_target_commune,
+)
 from e_cartomobile.data_transform.core import get_x_y_from_lat_lon
 
-POWER_CLUSTER = ['Low','Standard','Fast','Very Fast']
+POWER_CLUSTER = ["Low", "Standard", "Fast", "Very Fast"]
 
-#%%
-# Get the bornes count by commuens and power 
+
+# %%
+# Get the bornes count by commuens and power
 def compute_bornes_by_communes(df_irve_power_clean):
-
     df_cluster_plot = df_irve_power_clean.groupby(
         ["code_commune_INSEE", "cluster"]
-    ).agg("count")[
-        "puissance_nominale"
-    ] 
+    ).agg("count")["puissance_nominale"]
 
-    df_bornes_communes = df_cluster_plot.reset_index().pivot(index='code_commune_INSEE',columns='cluster')
-    df_bornes_communes.columns = POWER_CLUSTER # warning : depends on compute_power_cluster bins
+    df_bornes_communes = df_cluster_plot.reset_index().pivot(
+        index="code_commune_INSEE", columns="cluster"
+    )
+    df_bornes_communes.columns = (
+        POWER_CLUSTER  # warning : depends on compute_power_cluster bins
+    )
 
     return df_bornes_communes
+
 
 def compute_pdc_by_communes(df_irve_power_clean):
-
     df_cluster_plot = df_irve_power_clean.groupby(
         ["code_commune_INSEE", "cluster"]
-    ).agg("sum")[
-        "nbre_pdc"
-    ] 
+    ).agg("sum")["nbre_pdc"]
 
-    df_bornes_communes = df_cluster_plot.reset_index().pivot(index='code_commune_INSEE',columns='cluster')
-    df_bornes_communes.columns = POWER_CLUSTER # warning : depends on compute_power_cluster bins
+    df_bornes_communes = df_cluster_plot.reset_index().pivot(
+        index="code_commune_INSEE", columns="cluster"
+    )
+    df_bornes_communes.columns = (
+        POWER_CLUSTER  # warning : depends on compute_power_cluster bins
+    )
 
     return df_bornes_communes
 
 
-def compute_power_cluster(df_irve_power_clean, cluster_labels = POWER_CLUSTER):
+def compute_power_cluster(df_irve_power_clean, cluster_labels=POWER_CLUSTER):
     bins = pd.IntervalIndex.from_tuples(
         [
             (0, 7.4),
@@ -49,9 +55,10 @@ def compute_power_cluster(df_irve_power_clean, cluster_labels = POWER_CLUSTER):
     df_irve_power_cluster = pd.cut(df_irve_power_clean.puissance_nominale, bins)
 
     # Set the labels instead of the bins values
-    df_irve_power_cluster = df_irve_power_cluster.map(dict(zip(bins,cluster_labels)))
-    
+    df_irve_power_cluster = df_irve_power_cluster.map(dict(zip(bins, cluster_labels)))
+
     return df_irve_power_cluster
+
 
 def clean_power_values(gdf_irve):
     df_irve_power_clean = gdf_irve.copy()
@@ -61,22 +68,22 @@ def clean_power_values(gdf_irve):
     return df_irve_power_clean
 
 
-#%%
+# %%
 #  Combine the communes data
 def add_close_bornes_by_power_cluster_simple(df_bornes_communes, gamma, dist_max_km):
     """
     For a dedicated city, add the irve nearby, depending on the gamma and dist_max_km parameters.
 
     df_bornes_communes must contain x_crs_2154 and y_crs_2154 positions of the cities, in CRS 2154 reference.
-    
+
     Warning : this ponderation is simplified by taking all charging point the commune centers
-    
+
     """
     df_b = df_bornes_communes.copy()
 
     for cluster in df_b.columns:
-        if not(cluster.startswith('x') or  cluster.startswith('y')):
-            df_b[cluster+"_completed"] = df_b.apply(
+        if not (cluster.startswith("x") or cluster.startswith("y")):
+            df_b[cluster + "_completed"] = df_b.apply(
                 lambda x: score_4_target_commune(
                     gamma,
                     dist_max_km,
@@ -90,20 +97,20 @@ def add_close_bornes_by_power_cluster_simple(df_bornes_communes, gamma, dist_max
             )
     return df_b
 
+
 def add_close_pdc_by_power_cluster(df_bornes_communes, df_irve, gamma, dist_max_km):
     """
     For a dedicated city, add the irve nearby, depending on the gamma and dist_max_km parameters.
 
     df_irve must contain x_crs_2154 and y_crs_2154 positions, in CRS 2154 reference.
     df_irve must contain power clusters
-    
+
     """
     df_b = df_bornes_communes.copy()
     for cluster in df_b.columns:
-
         df_irve_extract = df_irve[df_irve.cluster == cluster]
-        if not(cluster.startswith('x') or  cluster.startswith('y')):
-            df_b[cluster+"_completed"] = df_b.apply(
+        if not (cluster.startswith("x") or cluster.startswith("y")):
+            df_b[cluster + "_completed"] = df_b.apply(
                 lambda x: score_4_target_commune(
                     gamma,
                     dist_max_km,
@@ -117,8 +124,8 @@ def add_close_pdc_by_power_cluster(df_bornes_communes, df_irve, gamma, dist_max_
             )
     return df_b
 
-def complete_df_irve(df_irve):
 
+def complete_df_irve(df_irve):
     # df_irve must contain power clusters
     # Create clusters
     df_irve_power_cluster = compute_power_cluster(df_irve)
@@ -127,67 +134,88 @@ def complete_df_irve(df_irve):
     df_irve["cluster"] = df_irve_power_cluster
 
     # df_irve must contain x_crs_2154 and y_crs_2154 positions of the cities, in CRS 2154 reference.
-    df_irve[["x_crs_2154","y_crs_2154"]] = np.array(get_x_y_from_lat_lon(
-                                df_irve.consolidated_latitude,
-                                df_irve.consolidated_longitude)
-                                ).T
-    
+    df_irve[["x_crs_2154", "y_crs_2154"]] = np.array(
+        get_x_y_from_lat_lon(
+            df_irve.consolidated_latitude, df_irve.consolidated_longitude
+        )
+    ).T
+
     return df_irve
 
-#%%
+
+# %%
 # apply pondaration depending on the scenario
 def compute_unique_bornes_ponderated(s_bornes_communes, weights) -> float:
+    return np.sum(
+        [s_bornes_communes.get(key, 0) * value for key, value in weights.items()]
+    )
 
-    return np.sum([s_bornes_communes.get(key,0)*value for key, value in weights.items()])
 
-
-def compute_bornes_ponderated(df_bornes_communes, weights = None) -> pd.Series:
-
+def compute_bornes_ponderated(df_bornes_communes, weights=None) -> pd.Series:
     # Deal with deafult values
-    if weights is None: # by default, 1 for each
-        weights = {cluster : 1 for cluster in df_bornes_communes.communes if cluster.contains('_completed')}
+    if weights is None:  # by default, 1 for each
+        weights = {
+            cluster: 1
+            for cluster in df_bornes_communes.communes
+            if cluster.contains("_completed")
+        }
 
     # Normalized the weights
     total_weight = np.sum([values for values in weights.values()])
-    weights_normalized = {key : value/total_weight for key, value in weights.items()}
-    
+    weights_normalized = {key: value / total_weight for key, value in weights.items()}
+
     # Apply weights
     s_bornes_communes_ponderated = df_bornes_communes.apply(
-        lambda x : compute_unique_bornes_ponderated(x,weights_normalized), 
-        axis=1
-        )
+        lambda x: compute_unique_bornes_ponderated(x, weights_normalized), axis=1
+    )
 
     return s_bornes_communes_ponderated
 
-#%%
-# Combine all
-def compute_bornes_by_communes_ponderated(gamma = 5, dist_max_km = 20):
 
+# %%
+# Combine all
+def compute_bornes_by_communes_smoothed(gamma=5, dist_max_km=20):
     # Read data in database
     gdf_irve = get_bornes_data()
 
     # Clean Power values
     df_irve = clean_power_values(gdf_irve)
-    # Get features for IRVE 
+    # Get features for IRVE
     df_irve = complete_df_irve(df_irve)
 
     # Compute bornes for each city
     df_bornes_communes = compute_pdc_by_communes(df_irve)
     # Complete city with position data
     gdf_comm = get_communes_data()
-    gdf_comm[["x_crs_2154","y_crs_2154"]] = np.array(get_x_y_from_lat_lon(gdf_comm.y,gdf_comm.x)).T
-    df_bornes_communes_completed = df_bornes_communes.join(gdf_comm[["insee","x_crs_2154","y_crs_2154"]].set_index("insee"))
+    gdf_comm[["x_crs_2154", "y_crs_2154"]] = np.array(
+        get_x_y_from_lat_lon(gdf_comm.y, gdf_comm.x)
+    ).T
+    df_bornes_communes_completed = df_bornes_communes.join(
+        gdf_comm[["insee", "x_crs_2154", "y_crs_2154"]].set_index("insee")
+    )
+    # TODO : gérer les villes à arrondissement pour avoir leur localisation : agg avant les bornes ?
 
-    # smooth 
-    df_bornes_communes_smooth = add_close_pdc_by_power_cluster(df_bornes_communes_completed, df_irve, gamma, dist_max_km)
+    # smooth
+    df_bornes_communes_smooth = add_close_pdc_by_power_cluster(
+        df_bornes_communes_completed, df_irve, gamma, dist_max_km
+    )
 
     return df_bornes_communes_smooth
 
-def compute_bornes_by_communes_ponderated(df_bornes_communes_smooth, scenario = 'default'):
 
-    return compute_bornes_ponderated(df_bornes_communes_smooth, get_scenario_weight(scenario))
+def compute_bornes_by_communes_ponderated(
+    df_bornes_communes_smooth, scenario="default"
+):
+    return compute_bornes_ponderated(
+        df_bornes_communes_smooth, get_scenario_weight(scenario)
+    )
 
 
 def get_scenario_weight(scenario):
     # Dummy fonction : to complete
-    return {x : 1 for x in POWER_CLUSTER}
+    if scenario in ["default", "smoothed_uniform"]:
+        return {x: 1 for x in [cluster + "_completed" for cluster in POWER_CLUSTER]}
+    elif scenario == "uniform":
+        return {x: 1 for x in POWER_CLUSTER}
+    else:
+        raise NotImplementedError(f"Le scénario {scenario} n'a pas de poids associés")
