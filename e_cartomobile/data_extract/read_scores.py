82aee317734b640bb08d8c54cbc901a38acb564d
Bastien <bastien.gauthier@ntymail.com>
2023-11-20 19:49:58 +0100

    Ajout de la fonction de calcul du score 2

--- e_cartomobile/data_extract/read_scores.py
+++ e_cartomobile/data_extract/read_scores.py
@@ -1,9 +1,12 @@
+# %%
+# Importation
 import pandas as pd
 from sklearn.preprocessing import QuantileTransformer
 
 from e_cartomobile.infra.database.sql_connection import get_db_connector
 
-
+# %%
+# Récupération des Scores
 def get_score_4(gamma: float, dist_max_km: float) -> pd.DataFrame:
     conn = get_db_connector()
 
@@ -18,6 +21,19 @@ def get_score_4(gamma: float, dist_max_km: float) -> pd.DataFrame:
     return output_score4
 
 
+def get_score_2() -> pd.DataFrame:
+    conn = get_db_connector()
+
+    req = "SELECT insee, score_2 FROM score_2"
+
+    output = pd.read_sql(req, conn)
+
+    output = output.set_index("insee").squeeze()
+    output.name = "score_2"
+
+    return output
+
+
 def get_score_1() -> pd.DataFrame:
     conn = get_db_connector()
 
@@ -30,7 +46,8 @@ def get_score_1() -> pd.DataFrame:
 
     return output
 
-
+# %%
+# Récupération des bornes
 def get_bornes_smoothed_uniform_scenario() -> pd.DataFrame:
     conn = get_db_connector()
 
@@ -43,7 +60,8 @@ def get_bornes_smoothed_uniform_scenario() -> pd.DataFrame:
 
     return output
 
-
+# %%
+# Calcul des besoins
 def compute_besoin_local() -> pd.DataFrame:
     bornes_smoothed_uniform_scenario = get_bornes_smoothed_uniform_scenario()
     score_4 = get_score_4(5, 20)
@@ -58,6 +76,27 @@ def compute_besoin_local() -> pd.DataFrame:
     )
 
     return s_besoin_local
+
+def compute_besoin_tourisme(esp = 1e-5) -> pd.DataFrame:
+    """
+    The esp value is necessary to impose a minimum value to the need of charging station
+    """
+    bornes_smoothed_uniform_scenario = get_bornes_smoothed_uniform_scenario()
+    score_2 = get_score_2()
+
+    besoin_tourisme = bornes_smoothed_uniform_scenario / (score_2 + esp)
+    besoin_tourisme = besoin_tourisme.fillna(0)
+
+    scaler_tourisme = QuantileTransformer()
+    besoin_tourisme_scaled = scaler_tourisme.fit_transform(
+        besoin_tourisme.values.reshape(-1, 1)
+    )
+
+    s_besoin_tourisme = pd.Series(
+        besoin_tourisme_scaled.T[0], index=besoin_tourisme.index, name="besoin_tourisme"
+    )
+
+    return s_besoin_tourisme
 
 
 def compute_besoin_reseau() -> pd.DataFrame:
